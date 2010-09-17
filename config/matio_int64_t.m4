tbeu/matio

commit 6e3290f309e27bab5497e71dcee5ac197b32c2b6

Fix a mistake

config/matio_int64_t.m4
@@ -28,7 +28,7 @@ AC_DEFUN([CHECK_MATIO_INT64_T],
         #endif
     ],
     [int64_t i = 0;],
-    ac_have_mat_int64_t=yes,ac_have_mat_int64_t=no)
+    ac_have_mat_int64_t =yes,ac_have_mat_int64_t=no)
 
     if test "x$ac_have_mat_int64_t" = "xyes"
     then
@@ -136,7 +136,7 @@ AC_DEFUN([CHECK_MATIO_INT64_T],
 
         if test "x$ac_have_mat_int64_t" = "xyes"
         then
-            AC_DEFINE_UNQUOTED([HAVE_MAT_INT64_T],[],[Have MAT int64]))
+            AC_DEFINE_UNQUOTED([HAVE_MAT_INT64_T],[],[Have MAT int64])
             AC_DEFINE_UNQUOTED([_mat_int64_t],[long],[int64 type])
             AC_MSG_RESULT([long])
         fi

doxygen/textopdf.sh
@@ -1,8 +1,22 @@
 #! /bin/bash
 
 pdflatex refman.tex
+if [ $? -ne 0 ]; then
+    echo "pdflatex failed"
+    exit 1
+fi
+
 makeindex refman.idx
+if [ $? -ne 0 ]; then
+    echo "makeindex failed"
+    exit 1
+fi
+
 pdflatex refman.tex
+if [ $? -ne 0 ]; then
+    echo "pdflatex failed"
+    exit 1
+fi
 
 latex_count=5;
 while egrep -s 'Rerun (LaTeX|to get cross-references right)' refman.log && [ $$latex_count -gt 0 ]; do

commit 290e7353bc3a6cc82e9e3356ec25e5bf9d1792a2

Fix syntax error introduced by 6e3290f309e27bab5497e71dcee5ac197b32c2b6
* config/matio_int64_t.m4 (CHECK_MATIO_INT64_T): Remove space between
      ac_have_mat_int64_t and equal sign in argument to AC_TRY_LINK

config/matio_int64_t.m4
@@ -28,7 +28,7 @@ AC_DEFUN([CHECK_MATIO_INT64_T],
         #endif
     ],
     [int64_t i = 0;],
-    ac_have_mat_int64_t =yes,ac_have_mat_int64_t=no)
+    ac_have_mat_int64_t=yes,ac_have_mat_int64_t=no)
 
     if test "x$ac_have_mat_int64_t" = "xyes"
     then