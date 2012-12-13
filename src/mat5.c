6af40d8434666e658649018b754beb53ab9cb4fd
Christopher Hulbert <chulbe2lsu@users.sourceforge.net>
2012-12-13 17:39:41 -0500

    Fix writing empty data to still write the data type
    * src/mat5.c (WriteData): Write the data type and 0 bytes even if data is
          NULL or there are 0 elements

--- src/mat5.c
+++ src/mat5.c
@@ -1470,14 +1470,16 @@ WriteData(mat_t *mat,void *data,int N,enum matio_types data_type)
 {
     int nBytes = 0, data_size;
 
-    if ((mat == NULL) || (mat->fp == NULL) || (data == NULL && N > 0))
+    if ((mat == NULL) || (mat->fp == NULL) )
         return 0;
 
     data_size = Mat_SizeOf(data_type);
     nBytes    = N*data_size;
     fwrite(&data_type,4,1,mat->fp);
     fwrite(&nBytes,4,1,mat->fp);
-    fwrite(data,data_size,N,mat->fp);
+
+    if ( data != NULL && N > 0 )
+        fwrite(data,data_size,N,mat->fp);
 
     return nBytes;
 }
