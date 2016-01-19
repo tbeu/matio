593efcab069f2e9668ed19b3276e6577b8616cd4
Nils Jannasch <nils.jannasch@tu-dortmund.de>
2016-01-19 19:47:37 +0100

    Doxygen warning removed

--- src/mat.c
+++ src/mat.c
@@ -349,7 +349,7 @@ Mat_Close( mat_t *mat )
  *
  * Gets the filename for the given MAT file
  * @ingroup MAT
- * @param mat Pointer to the MAT file
+ * @param matfp Pointer to the MAT file
  * @return MAT filename
  */
 const char *
@@ -365,7 +365,7 @@ Mat_GetFilename(mat_t *matfp)
  *
  * Gets the version of the given MAT file
  * @ingroup MAT
- * @param mat Pointer to the MAT file
+ * @param matfp Pointer to the MAT file
  * @return MAT file version
  */
 enum mat_ft
