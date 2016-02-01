8109b14a1f0c268443288f48ac4a69347c6e4873
tbeu <tbeu@users.noreply.github.com>
2016-02-01 19:38:17 +0100

    Add license header to coverity_model.c

--- coverity_model.c
+++ coverity_model.c
@@ -1,4 +1,33 @@
-/* Modeling file for Coverity Scan to find resource leaks using hdf5 and zlib */
+/** @file coverity_model.c
+ * @brief Modeling file for Coverity Scan to find resource leaks using hdf5 and zlib
+ */
+/*
+ * Copyright (C) 2016   Thomas Beutlich
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CHRISTOPHER C. HULBERT ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL CHRISTOPHER C. HULBERT OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #define hid_t void*
 #define hsize_t size_t
@@ -114,7 +143,9 @@ herr_t H5Sclose(hid_t id) {
 }
 
 hid_t H5Rdereference(hid_t dataset, H5R_type_t ref_type, const void *ref) {
-    return __coverity_alloc__(10);
+    hid_t id = __coverity_alloc__(10);
+    __coverity_mark_as_afm_allocated__(id, "H5Dclose");
+    return id;
 }
 
 hid_t H5Aget_type(hid_t attr_id) {
