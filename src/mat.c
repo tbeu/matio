1433636e18e15b143ba1b824481a96c59d5f251d
tbeu <tbeu@users.noreply.github.com>
2016-02-08 21:38:18 +0100

    Set empty keyword for tests on empty data

--- src/mat.c
+++ src/mat.c
@@ -1451,7 +1451,6 @@ Mat_VarPrint( matvar_t *matvar, int printdata )
         }
         return;
     } else if ( matvar->data == NULL || matvar->data_size < 1 ) {
-        printf("{\n}\n");
         return;
     } else if ( MAT_C_CELL == matvar->class_type ) {
         matvar_t **cells = (matvar_t **)matvar->data;
