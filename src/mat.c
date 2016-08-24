778123bec2496dd02060ec5c67d4dc81e1d49794
Greg Sjaardema <gsjaardema@gmail.com>
2016-08-24 11:58:19 -0600

    Eliminate use of mktemp()
    The use of mktemp() results in an unsuppressable warning message
    on certain compilers, including gcc.  This causes problems for
    projects which have rules regarding warning-free compiles.
    
    Use mkstemp instead. The problem with mkstemp is that it opens the
    file instead of just returning a unique file name.  However, as a
    side effect it does change the template to a unique file name.
    This can be used similarly to the return value from mktemp.

--- src/mat.c
+++ src/mat.c
@@ -860,11 +860,13 @@ Mat_VarDelete(mat_t *mat, const char *name)
     int   err = 1;
     char *tmp_name;
     char temp[7] = "XXXXXX";
+    int  fd;
 
     if ( NULL == mat || NULL == name )
         return err;
 
-    if ( (tmp_name = mktemp(temp)) != NULL ) {
+    fd = mkstemp(temp); /* NOTE: This changes temp */
+    if (fd > 0) {
         enum mat_ft mat_file_ver;
         mat_t *tmp;
 
@@ -883,7 +885,8 @@ Mat_VarDelete(mat_t *mat, const char *name)
                 break;
         }
 
-        tmp = Mat_CreateVer(tmp_name,mat->header,mat_file_ver);
+        close(fd);
+        tmp = Mat_CreateVer(temp,mat->header,mat_file_ver);
         if ( tmp != NULL ) {
             matvar_t *matvar;
             Mat_Rewind(mat);
@@ -912,11 +915,11 @@ Mat_VarDelete(mat_t *mat, const char *name)
                     mat->fp = NULL;
                 }
 
-                if ( (err = mat_copy(tmp_name,new_name)) == -1 ) {
+                if ( (err = mat_copy(temp,new_name)) == -1 ) {
                     Mat_Critical("Cannot copy file from \"%s\" to \"%s\".",
-                        tmp_name, new_name);
-                } else if ( (err = remove(tmp_name)) == -1 ) {
-                    Mat_Critical("Cannot remove file \"%s\".",tmp_name);
+                        temp, new_name);
+                } else if ( (err = remove(temp)) == -1 ) {
+                    Mat_Critical("Cannot remove file \"%s\".",temp);
                 } else {
                     tmp = Mat_Open(new_name,mat->mode);
                     if ( NULL != tmp ) {
@@ -933,8 +936,8 @@ Mat_VarDelete(mat_t *mat, const char *name)
                     }
                 }
                 free(new_name);
-            } else if ( (err = remove(tmp_name)) == -1 ) {
-                Mat_Critical("Cannot remove file \"%s\".",tmp_name);
+            } else if ( (err = remove(temp)) == -1 ) {
+                Mat_Critical("Cannot remove file \"%s\".",temp);
             }
         }
     } else {
