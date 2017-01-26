9f8675ce42c66b53f37a51b8f980d97e056bfb7a
tbeu <tc@tbeu.de>
2017-01-26 08:25:21 +0100

    Also test latest zlib on Travis CI

--- .install_hdf5_1_10.sh
+++ .install_hdf5_1_10.sh
@@ -2,5 +2,5 @@
 
 svn co -q https://svn.hdfgroup.org/hdf5/branches/hdf5_1_10_0@29954
 cd hdf5_1_10_0
-./configure --quiet --enable-shared --enable-build-mode=debug --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-memory-alloc-sanity-check --disable-instrument --disable-parallel --disable-trace --disable-internal-debug --enable-optimization=debug --disable-asserts --with-pic --with-default-api-version=v110 CFLAGS="-w"
+./configure --quiet --enable-shared --enable-build-mode=debug --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-memory-alloc-sanity-check --disable-instrument --disable-parallel --disable-trace --disable-internal-debug --enable-optimization=debug --disable-asserts --with-pic --with-default-api-version=v110 --with-zlib=$TRAVIS_BUILD_DIR/zlib CFLAGS="-w"
 make install -C src
