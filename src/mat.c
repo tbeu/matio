3d776db344637a3eb8da750fd49a374b1d93f86b
tbeu <tc@tbeu.de>
2017-03-05 22:27:24 +0100

    Fix Mat_VarGetSize for cell/struct array with empty cells/fields

--- src/mat.c
+++ src/mat.c
@@ -1637,15 +1637,16 @@ size_t
 Mat_VarGetSize(matvar_t *matvar)
 {
     int i;
-    size_t bytes = 0;
-    size_t overhead = 0;
+    size_t bytes = 0, overhead = 0, ptr = 0;
 
 #if defined(_WIN64) || (defined(__SIZEOF_POINTER__) && (__SIZEOF_POINTER__ == 8)) || (defined(SIZEOF_VOID_P) && (SIZEOF_VOID_P == 8))
     /* 112 bytes cell/struct overhead for 64-bit system */
     overhead = 112;
+    ptr = 8;
 #elif defined(_WIN32) || (defined(__SIZEOF_POINTER__) && (__SIZEOF_POINTER__ == 4)) || (defined(SIZEOF_VOID_P) && (SIZEOF_VOID_P == 4))
     /* 60 bytes cell/struct overhead for 32-bit system */
     overhead = 60;
+    ptr = 4;
 #endif
 
     if ( matvar->class_type == MAT_C_STRUCT ) {
@@ -1659,7 +1660,10 @@ Mat_VarGetSize(matvar_t *matvar)
                 bytes = nmemb*nfields*overhead;
                 for ( i = 0; i < nmemb*nfields; i++ )
                     if ( NULL != fields[i] )
-                        bytes += Mat_VarGetSize(fields[i]);
+                        if ( MAT_C_EMPTY != fields[i]->class_type )
+                            bytes += Mat_VarGetSize(fields[i]);
+                        else
+                            bytes += ptr - overhead;
             }
         }
         bytes += 64 /* max field name length */ *nfields;
@@ -1668,9 +1672,14 @@ Mat_VarGetSize(matvar_t *matvar)
         if ( NULL != cells ) {
             int ncells = matvar->nbytes / matvar->data_size;
             bytes = ncells*overhead;
-            for ( i = 0; i < ncells; i++ )
-                if ( NULL != cells[i] )
-                    bytes += Mat_VarGetSize(cells[i]);
+            for ( i = 0; i < ncells; i++ ) {
+                if ( NULL != cells[i] ) {
+                    if ( MAT_C_EMPTY != cells[i]->class_type )
+                        bytes += Mat_VarGetSize(cells[i]);
+                    else
+                        bytes += ptr - overhead;
+                }
+            }
         }
     } else if ( matvar->class_type == MAT_C_SPARSE ) {
         mat_sparse_t *sparse = (mat_sparse_t*)matvar->data;
