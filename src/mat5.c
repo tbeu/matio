8863ff996164452944ef988f0d33eda6af3726d5
tbeu <tc@tbeu.de>
2017-11-09 09:36:02 +0100

    Remove duplicated code
    As reported by Better Code Hub

--- src/mat5.c
+++ src/mat5.c
@@ -533,9 +533,9 @@ WriteCompressedCharData(mat_t *mat,z_streamp z,void *data,int N,
 
     switch ( data_type ) {
         case MAT_T_UINT16:
-        {
-            data_size = 2;
-            data_tag[0] = MAT_T_UINT16;
+        case MAT_T_UTF8:
+            data_size = Mat_SizeOf(data_type);
+            data_tag[0] = data_type;
             data_tag[1] = N*data_size;
             z->next_in  = ZLIB_BYTE_PTR(data_tag);
             z->avail_in = 8;
@@ -570,7 +570,6 @@ WriteCompressedCharData(mat_t *mat,z_streamp z,void *data,int N,
                 } while ( z->avail_out == 0 );
             }
             break;
-        }
         case MAT_T_INT8:
         case MAT_T_UINT8:
         {
@@ -623,50 +622,8 @@ WriteCompressedCharData(mat_t *mat,z_streamp z,void *data,int N,
             }
             break;
         }
-        case MAT_T_UTF8:
-        {
-            data_size = 1;
-            data_tag[0] = MAT_T_UTF8;
-            data_tag[1] = N*data_size;
-            z->next_in  = ZLIB_BYTE_PTR(data_tag);
-            z->avail_in = 8;
-            do {
-                z->next_out  = buf;
-                z->avail_out = buf_size;
-                deflate(z,Z_NO_FLUSH);
-                byteswritten += fwrite(buf,1,buf_size-z->avail_out,(FILE*)mat->fp);
-            } while ( z->avail_out == 0 );
-
-            /* exit early if this is an empty data */
-            if ( NULL == data || N < 1 )
-                break;
-
-            z->next_in  = (Bytef*)data;
-            z->avail_in = data_size*N;
-            do {
-                z->next_out  = buf;
-                z->avail_out = buf_size;
-                deflate(z,Z_NO_FLUSH);
-                byteswritten += fwrite(buf,1,buf_size-z->avail_out,(FILE*)mat->fp);
-            } while ( z->avail_out == 0 );
-            /* Add/Compress padding to pad to 8-byte boundary */
-            if ( N*data_size % 8 ) {
-                z->next_in  = pad;
-                z->avail_in = 8 - (N*data_size % 8);
-                do {
-                    z->next_out  = buf;
-                    z->avail_out = buf_size;
-                    deflate(z,Z_NO_FLUSH);
-                    byteswritten += fwrite(buf,1,buf_size-z->avail_out,(FILE*)mat->fp);
-                } while ( z->avail_out == 0 );
-            }
-            break;
-        }
         case MAT_T_UNKNOWN:
-        {
-            /* Sometimes empty char data will have MAT_T_UNKNOWN, so just write
-             * a data tag
-             */
+            /* Sometimes empty char data will have MAT_T_UNKNOWN, so just write a data tag */
             data_size = 2;
             data_tag[0] = MAT_T_UINT16;
             data_tag[1] = N*data_size;
@@ -678,7 +635,7 @@ WriteCompressedCharData(mat_t *mat,z_streamp z,void *data,int N,
                 deflate(z,Z_NO_FLUSH);
                 byteswritten += fwrite(buf,1,buf_size-z->avail_out,(FILE*)mat->fp);
             } while ( z->avail_out == 0 );
-        }
+            break;
         default:
             break;
     }
