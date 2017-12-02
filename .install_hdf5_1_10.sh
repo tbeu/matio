d0a5ac81289d40d91a1666a9d94e29ef8add57d9
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2017-12-02 21:58:30 +0100

    Build hdf5 with cmake to get the proper hdf5 detection files.

--- .install_hdf5_1_10.sh
+++ .install_hdf5_1_10.sh
@@ -1,6 +1,11 @@
 #! /bin/sh 
 
-git clone --branch hdf5_1_10_1 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_10_1
+git clone --depth=1 --branch hdf5_1_10_1 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_10_1
 cd hdf5_1_10_1
-./configure --quiet --enable-shared --enable-build-mode=debug --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-memory-alloc-sanity-check --disable-instrument --disable-parallel --disable-trace --disable-internal-debug --enable-optimization=debug --disable-asserts --with-pic --with-default-api-version=v110 --with-zlib=$TRAVIS_BUILD_DIR/zlib CFLAGS="-w"
-make install -C src
+# To get the same shared library name as with autoconf
+sed -i -e's/-shared//' CMakeLists.txt
+mkdir build
+cd build
+cmake -DHDF5_ENABLE_Z_LIB_SUPPORT=ON -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR/hdf5 -DZLIB_DIR=$TRAVIS_BUILD_DIR/zlib -DBUILD_SHARED_LIBS=ON -DHDF5_BUILD_CPP_LIB=OFF -DHDF5_BUILD_EXAMPLES=OFF -DBUILD_TESTING:BOOL=OFF -DHDF5_BUILD_HL_LIB=OFF -DHDF5_BUILD_TOOLS=OFF -DHDF5_ENABLE_DEPRECATED_SYMBOLS=OFF ..
+#./configure --quiet --enable-shared --enable-build-mode=debug --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-memory-alloc-sanity-check --disable-instrument --disable-parallel --disable-trace --disable-internal-debug --enable-optimization=debug --disable-asserts --with-pic --with-default-api-version=v110 --with-zlib=$TRAVIS_BUILD_DIR/zlib CFLAGS="-w"
+make install
--- .install_hdf5_1_8.sh
+++ .install_hdf5_1_8.sh
@@ -1,6 +1,11 @@
 #! /bin/sh 
 
-git clone --branch hdf5_1_8_19 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_8_19
+git clone --depth 1 --branch hdf5_1_8_19 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_8_19
 cd hdf5_1_8_19
-./configure --quiet --enable-shared --disable-production --enable-debug=all --with-pic --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-clear-file-buffers --disable-instrument --disable-parallel --disable-trace --with-default-api-version=v18 CFLAGS="-w"
-make install -C src
+# To get the same shared library name as with autoconf
+sed -i -e's/-shared//' CMakeLists.txt
+mkdir build
+cd build
+cmake -DHDF5_ENABLE_Z_LIB_SUPPORT=ON -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR/hdf5 -DZLIB_DIR=$TRAVIS_BUILD_DIR/zlib -DBUILD_SHARED_LIBS=ON -DHDF5_BUILD_CPP_LIB=OFF -DHDF5_BUILD_EXAMPLES=OFF -DBUILD_TESTING:BOOL=OFF -DHDF5_BUILD_HL_LIB=OFF -HDF5_LIBSH_TARGET=hdf5 -DHDF5_BUILD_TOOLS=OFF -DHDF5_ENABLE_DEPRECATED_SYMBOLS=OFF ..
+#./configure --quiet --enable-shared --disable-production --enable-debug=all --with-pic --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-clear-file-buffers --disable-instrument --disable-parallel --disable-trace --with-default-api-version=v18 CFLAGS="-w"
+make install
