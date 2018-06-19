e3ad83edeb8fe2bca190e733242ee62dab3da9d9
tbeu <tc@tbeu.de>
2018-06-19 08:01:35 +0200

    Update to HDF5 1.8.21 at Travis CI

--- .install_hdf5_1_8.sh
+++ .install_hdf5_1_8.sh
@@ -1,6 +1,6 @@
 #! /bin/sh 
 
-git clone --depth 1 --branch hdf5_1_8_20 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_8_20
-cd hdf5_1_8_20
+git clone --depth 1 --branch hdf5_1_8_21 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_8_21
+cd hdf5_1_8_21
 ./configure --quiet --enable-shared --disable-production --enable-debug=all --with-pic --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-clear-file-buffers --disable-instrument --disable-parallel --disable-trace --with-default-api-version=v18 CFLAGS="-w"
 make install -C src
