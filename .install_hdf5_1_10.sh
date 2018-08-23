b32c4272912a3de7e26b402f3f44816e0027c647
tbeu <tc@tbeu.de>
2018-08-23 07:51:28 +0200

    Update to HDF5 1.10.3 at Travis CI

--- .install_hdf5_1_10.sh
+++ .install_hdf5_1_10.sh
@@ -1,6 +1,6 @@
 #! /bin/sh 
 
-git clone --depth 1 --branch hdf5_1_10_2 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_10_2
-cd hdf5_1_10_2
+git clone --depth 1 --branch hdf5_1_10_3 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_10_3
+cd hdf5_1_10_3
 ./configure --quiet --enable-shared --enable-build-mode=debug --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-memory-alloc-sanity-check --disable-instrument --disable-parallel --disable-trace --disable-internal-debug --enable-optimization=debug --disable-asserts --with-pic --with-default-api-version=v110 --with-zlib=$TRAVIS_BUILD_DIR/zlib CFLAGS="-w"
 make install -C src
