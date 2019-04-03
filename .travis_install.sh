c6fe8683577de5a74e0666e82761dbf1aa8ffc63
Joan Massich <sik@visor.udg.edu>
2019-04-03 10:48:06 +0200

    I don't find branch 1_10_5

--- .travis_install.sh
+++ .travis_install.sh
@@ -4,7 +4,9 @@ if [[ "$HDF5_VERSION_DIR" == "hdf5_1_10_5" ]]; then
 	git clone --depth 1 https://github.com/madler/zlib
 	pushd zlib && ./configure --prefix="$TRAVIS_BUILD_DIR"/zlib --eprefix="$TRAVIS_BUILD_DIR"/zlib && make install && popd
 	git clone --depth 1 --branch hdf5-1_10_5 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_10_5
-	pushd hdf5_1_10_5 && ./configure --quiet --enable-shared --enable-build-mode=debug --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-memory-alloc-sanity-check --disable-instrument --disable-parallel --disable-trace --disable-internal-debug --enable-optimization=debug --disable-asserts --with-pic --with-default-api-version=v110 --with-zlib="$TRAVIS_BUILD_DIR"/zlib CFLAGS="-w" && make install -C src && popd
+	pushd hdf5_1_10_5
+  git branch --all
+  ./configure --quiet --enable-shared --enable-build-mode=debug --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-memory-alloc-sanity-check --disable-instrument --disable-parallel --disable-trace --disable-internal-debug --enable-optimization=debug --disable-asserts --with-pic --with-default-api-version=v110 --with-zlib="$TRAVIS_BUILD_DIR"/zlib CFLAGS="-w" && make install -C src && popd
 fi
 
 if [[ "$HDF5_VERSION_DIR" == "hdf5_1_8_21" ]]; then
