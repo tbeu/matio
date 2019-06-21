9f46d2cbef189060b321d192cbf41c1d096a0e3b
tbeu <tbeu@users.noreply.github.com>
2019-06-21 12:26:56 +0200

    Check integer multiplication for overflow

--- src/mat4.c
+++ src/mat4.c
@@ -709,7 +709,7 @@ int
 Mat_VarReadDataLinear4(mat_t *mat,matvar_t *matvar,void *data,int start,
                        int stride,int edge)
 {
-    int err = 0;
+    int err;
     size_t nelems = 1;
 
     err = SafeMulDims(matvar, &nelems);
@@ -726,14 +726,18 @@ Mat_VarReadDataLinear4(mat_t *mat,matvar_t *matvar,void *data,int start,
         return 1;
     }
     if ( matvar->isComplex ) {
-            mat_complex_split_t *complex_data = (mat_complex_split_t*)data;
-            long nbytes = nelems*matvar->data_size;
+        mat_complex_split_t *complex_data = (mat_complex_split_t*)data;
+        err = SafeMul(&nelems, nelems, matvar->data_size);
+        if ( err ) {
+            Mat_Critical("Integer multiplication overflow");
+            return err;
+        }
 
-            ReadDataSlab1(mat,complex_data->Re,matvar->class_type,
-                          matvar->data_type,start,stride,edge);
-            (void)fseek((FILE*)mat->fp,matvar->internal->datapos+nbytes,SEEK_SET);
-            ReadDataSlab1(mat,complex_data->Im,matvar->class_type,
-                          matvar->data_type,start,stride,edge);
+        ReadDataSlab1(mat,complex_data->Re,matvar->class_type,
+                      matvar->data_type,start,stride,edge);
+        (void)fseek((FILE*)mat->fp,matvar->internal->datapos+nelems,SEEK_SET);
+        ReadDataSlab1(mat,complex_data->Im,matvar->class_type,
+                      matvar->data_type,start,stride,edge);
     } else {
         ReadDataSlab1(mat,data,matvar->class_type,matvar->data_type,start,
                       stride,edge);
