d616904ad41f71a394780bda76f9585485d854a4
tbeu <tbeu@users.noreply.github.com>
2019-11-23 22:15:13 +0100

    Avoid stack-overflow
    As reported by https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=19035

--- src/mat5.c
+++ src/mat5.c
@@ -1663,8 +1663,18 @@ ReadNextFunctionHandle(mat_t *mat, matvar_t *matvar)
     if ( matvar->data != NULL ) {
         size_t i;
         matvar_t **functions = (matvar_t**)matvar->data;
-        for ( i = 0; i < nelems; i++ )
+        for ( i = 0; i < nelems; i++ ) {
             functions[i] = Mat_VarReadNextInfo(mat);
+            err = NULL == functions[i];
+            if ( err )
+                break;
+        }
+        if ( err ) {
+            free(matvar->data);
+            matvar->data = NULL;
+            matvar->data_size = 0;
+            matvar->nbytes    = 0;
+        }
     } else {
         matvar->data_size = 0;
         matvar->nbytes    = 0;
