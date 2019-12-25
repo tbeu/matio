47ccdf88e0afe6456e23c2e2e8c402d296f63643
tbeu <tbeu@users.noreply.github.com>
2019-12-25 11:02:56 +0100

    Update to HDF5 1.10.6 at Travis CI

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ "$HDF5_VERSION_DIR" == "hdf5_1_10_5" ]]; then
+if [[ "$HDF5_VERSION_DIR" == "hdf5_1_10_6" ]]; then
 	git clone --depth 1 https://github.com/madler/zlib
 	pushd zlib && ./configure --prefix="$TRAVIS_BUILD_DIR"/zlib --eprefix="$TRAVIS_BUILD_DIR"/zlib && make install && popd
-	git clone --depth 1 --branch hdf5-1_10_5 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_10_5
-	pushd hdf5_1_10_5 && ./configure --quiet --enable-shared --enable-build-mode=debug --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-memory-alloc-sanity-check --disable-instrument --disable-parallel --disable-trace --disable-internal-debug --enable-optimization=debug --disable-asserts --with-pic --with-default-api-version=v110 --with-zlib="$TRAVIS_BUILD_DIR"/zlib CFLAGS="-w" && make install -C src && popd
+	git clone --depth 1 --branch hdf5-1_10_6 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_10_6
+	pushd hdf5_1_10_6 && ./configure --quiet --enable-shared --enable-build-mode=debug --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-memory-alloc-sanity-check --disable-instrument --disable-parallel --disable-trace --disable-internal-debug --enable-optimization=debug --disable-asserts --with-pic --with-default-api-version=v110 --with-zlib="$TRAVIS_BUILD_DIR"/zlib CFLAGS="-w" && make install -C src && popd
 fi
 
 if [[ "$HDF5_VERSION_DIR" == "hdf5_1_8_21" ]]; then
