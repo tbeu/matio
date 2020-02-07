596cb3ce71038958812bd6cf9b141f12ce155ac6
tbeu <tbeu@users.noreply.github.com>
2020-02-07 19:14:27 +0100

    Test HDF5 1.12.0

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ "$HDF5_VERSION_DIR" == "hdf5_1_12_0" ]]; then
+	git clone --depth 1 https://github.com/madler/zlib
+	pushd zlib && ./configure --prefix="$TRAVIS_BUILD_DIR"/zlib --eprefix="$TRAVIS_BUILD_DIR"/zlib && make install && popd
+	git clone --depth 1 --branch hdf5_1_12_0 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_12_0
+	pushd hdf5_1_12_0 && ./configure --quiet --enable-shared --enable-build-mode=debug --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-memory-alloc-sanity-check --disable-instrument --disable-parallel --disable-trace --disable-internal-debug --enable-optimization=debug --disable-asserts --disable-tests --disable-tools --with-pic --with-default-api-version=v112 --with-zlib="$TRAVIS_BUILD_DIR"/zlib CFLAGS="-w" && make install -C src && popd
+fi
+
 if [[ "$HDF5_VERSION_DIR" == "hdf5_1_10_6" ]]; then
 	git clone --depth 1 https://github.com/madler/zlib
 	pushd zlib && ./configure --prefix="$TRAVIS_BUILD_DIR"/zlib --eprefix="$TRAVIS_BUILD_DIR"/zlib && make install && popd
