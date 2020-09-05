913d8000e2fbe3880b7035bc41cd32eacd8238c3
Maarten Bent <MaartenBent@users.noreply.github.com>
2020-09-05 12:50:38 +0200

    Fix using hdf5-config from HDF5
    The config files require an exact version match. Also use the provided targets directly.
    Define H5_BUILT_AS_DYNAMIC_LIB when HDF5 1.8 config file is used.

--- cmake/thirdParties.cmake
+++ cmake/thirdParties.cmake
@@ -1,6 +1,9 @@
 if(MATIO_WITH_HDF5)
-    find_package(HDF5 1.8)
+    find_package(HDF5)
     if(HDF5_FOUND)
+        if(HDF5_VERSION VERSION_LESS 1.8)
+            message(FATAL_ERROR "HDF5 version ${HDF5_VERSION} is unsupported, minimum of 1.8 is required")
+        endif()
         set(HAVE_HDF5 1)
     endif()
 endif()
@@ -14,10 +17,24 @@ endif()
 
 if(HDF5_FOUND)
     add_library(HDF5::HDF5 INTERFACE IMPORTED)
-    set_target_properties(HDF5::HDF5 PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${HDF5_INCLUDE_DIRS}"
-        INTERFACE_LINK_LIBRARIES "${HDF5_LIBRARIES}"
-    )
+    if(HDF5_USE_STATIC_LIBRARIES AND TARGET hdf5::hdf5-static)
+        # static target from hdf5 1.10 or 1.12 config
+        target_link_libraries(HDF5::HDF5 INTERFACE hdf5::hdf5-static)
+    elseif(NOT HDF5_USE_STATIC_LIBRARIES AND TARGET hdf5::hdf5-shared)
+        # shared target from hdf5 1.10 or 1.12 config
+        target_link_libraries(HDF5::HDF5 INTERFACE hdf5::hdf5-shared)
+    elseif(TARGET hdf5)
+        # target from hdf5 1.8 config
+        target_link_libraries(HDF5::HDF5 INTERFACE hdf5)
+        if(NOT HDF5_USE_STATIC_LIBRARIES)
+            target_compile_definitions(HDF5::HDF5 INTERFACE "H5_BUILT_AS_DYNAMIC_LIB")
+        endif()
+    else()
+        # results from CMake FindHDF5
+        target_include_directories(HDF5::HDF5 INTERFACE "${HDF5_INCLUDE_DIRS}")
+        target_link_libraries(HDF5::HDF5 INTERFACE "${HDF5_LIBRARIES}")
+        target_compile_definitions(HDF5::HDF5 INTERFACE "${HDF5_DEFINITIONS}")
+    endif()
 endif()
 
 if(NOT HAVE_HDF5 AND MATIO_MAT73)
