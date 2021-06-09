8b220fde8de36c7baed8feb908a88e03a58d055d
Maarten Bent <MaartenBent@users.noreply.github.com>
2021-06-09 20:29:57 +0200

    Add macOS CMake build to TravisCI.
    Cache homebrew directory for faster build.

--- .ci/travis_before_script.sh
+++ .ci/travis_before_script.sh
@@ -17,32 +17,28 @@ if [[ "${USE_CMAKE:-no}" == "no" ]]; then
         CC=$(which clang)
     fi
 fi
-if [[ "${USE_CMAKE:-no}" == "yes" ]] &&  [[ "${USE_CONAN:-no}" == "no" ]]; then
-    SRC_DIR=$TRAVIS_BUILD_DIR
-    BUILD_DIR=$HOME/matio_cmake
-    HDF5_DIR=$HOME/CMake-hdf5-$HDF5_VERSION/HDF5-$HDF5_VERSION-Linux/HDF_Group/HDF5/$HDF5_VERSION
-    mkdir -p $BUILD_DIR
-    pushd $BUILD_DIR
-    cmake $SRC_DIR -DCMAKE_BUILD_TYPE=Debug \
-                   -DCMAKE_PREFIX_PATH=$HDF5_DIR/share/cmake \
-                   -DMATIO_EXTENDED_SPARSE=$ENABLE_EXTENDED_SPARSE \
-                   -DMATIO_MAT73=$ENABLE_MAT73 \
-                   -DMATIO_WITH_HDF5=$ENABLE_MAT73 \
-                   -DMATIO_WITH_ZLIB=$WITH_ZLIB \
-                   -DMATIO_SHARED=TRUE
-    popd
-fi
-if [[ "${USE_CMAKE:-no}" == "yes" ]] &&  [[ "${USE_CONAN:-no}" == "yes" ]]; then
+if [[ "${USE_CMAKE:-no}" == "yes" ]]; then
+    if [[ "$TRAVIS_OS_NAME" == "linux" ]] && [[ "${USE_CONAN:-no}" == "no" ]]; then
+        HDF5_DIR=$HOME/CMake-hdf5-$HDF5_VERSION/HDF5-$HDF5_VERSION-Linux/HDF_Group/HDF5/$HDF5_VERSION
+        CMAKE_PREFIX_PATH=-DCMAKE_PREFIX_PATH=$HDF5_DIR/share/cmake
+    fi
+
+    if [[ "${USE_CONAN:-no}" == "yes" ]]; then
+        CMAKE_CONAN_ARG=-DMATIO_USE_CONAN=TRUE
+    fi
+
     SRC_DIR=$TRAVIS_BUILD_DIR
     BUILD_DIR=$HOME/matio_cmake
     mkdir -p $BUILD_DIR
     pushd $BUILD_DIR
+
     cmake $SRC_DIR -DCMAKE_BUILD_TYPE=Debug \
+                   $CMAKE_PREFIX_PATH \
                    -DMATIO_EXTENDED_SPARSE=$ENABLE_EXTENDED_SPARSE \
                    -DMATIO_MAT73=$ENABLE_MAT73 \
                    -DMATIO_WITH_HDF5=$ENABLE_MAT73 \
                    -DMATIO_WITH_ZLIB=$WITH_ZLIB \
                    -DMATIO_SHARED=TRUE \
-                   -DMATIO_USE_CONAN=TRUE
+                   $CMAKE_CONAN_ARG
     popd
 fi
