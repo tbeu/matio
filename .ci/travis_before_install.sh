401dcf6c3d66697ad5d9fc8852fe1ae1715ce81b
tbeu <tbeu@users.noreply.github.com>
2021-06-11 20:48:11 +0200

    Fix USE_CONAN at Travis CI

--- .ci/travis_before_install.sh
+++ .ci/travis_before_install.sh
@@ -8,7 +8,7 @@ if [[ "$COVERITY_SCAN_BRANCH" != 1 ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]] && [
 fi
 
 if [[ "${USE_CONAN:-no}" == "yes" ]]; then
-    pip install --user conan==1.29.0
+    pip3 install --user conan
 fi
 
 if [[ "$COVERITY_SCAN_BRANCH" != 1 ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]] && [[ "$TRAVIS_CPU_ARCH" == "amd64" ]] && [[ "$CC" == "gcc-4.8" ]] && [[ "$HDF5_VERSION" == "1.8.22" ]] && [[ "$ENABLE_MAT73" == "yes" ]] && [[ "$ENABLE_EXTENDED_SPARSE" == "yes" ]] && [[ "$WITH_ZLIB" == "yes" ]] && [[ "$MAX_RANK" == 3 ]]; then
