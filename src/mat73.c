d94792d5e7222f4f17831197f3d36e12cf852ad4
tbeu <tbeu@users.noreply.github.com>
2023-10-26 18:26:50 +0200

    Fix memory leak
    As reported by https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=63617

--- src/mat73.c
+++ src/mat73.c
@@ -625,8 +625,7 @@ static int
 Mat_H5ReadFieldNames(matvar_t *matvar, hid_t dset_id, hsize_t *nfields)
 {
     hsize_t i;
-    hid_t field_id, attr_id, space_id;
-    hvl_t *fieldnames_vl;
+    hid_t attr_id, space_id;
     herr_t herr;
     int err;
 
@@ -638,52 +637,52 @@ Mat_H5ReadFieldNames(matvar_t *matvar, hid_t dset_id, hsize_t *nfields)
         H5Aclose(attr_id);
         return MATIO_E_GENERIC_READ_ERROR;
     } else {
-        if ( err == 0 ) {
-            *nfields = 1;
-        }
         err = MATIO_E_NO_ERROR;
     }
-    fieldnames_vl = (hvl_t *)calloc((size_t)(*nfields), sizeof(*fieldnames_vl));
-    if ( fieldnames_vl == NULL ) {
-        H5Sclose(space_id);
-        H5Aclose(attr_id);
-        return MATIO_E_OUT_OF_MEMORY;
-    }
-    field_id = H5Aget_type(attr_id);
-    herr = H5Aread(attr_id, field_id, fieldnames_vl);
-    if ( herr >= 0 ) {
-        matvar->internal->num_fields = (unsigned int)*nfields;
-        matvar->internal->fieldnames =
-            (char **)calloc((size_t)(*nfields), sizeof(*matvar->internal->fieldnames));
-        if ( matvar->internal->fieldnames != NULL ) {
-            for ( i = 0; i < *nfields; i++ ) {
-                matvar->internal->fieldnames[i] = (char *)calloc(fieldnames_vl[i].len + 1, 1);
-                if ( matvar->internal->fieldnames[i] != NULL ) {
-                    if ( fieldnames_vl[i].p != NULL ) {
-                        memcpy(matvar->internal->fieldnames[i], fieldnames_vl[i].p,
-                               fieldnames_vl[i].len);
-                    }
-                } else {
-                    err = MATIO_E_OUT_OF_MEMORY;
-                    break;
-                }
-            }
-        } else {
-            err = MATIO_E_OUT_OF_MEMORY;
+    if ( *nfields > 0 ) {
+        hid_t field_id;
+        hvl_t *fieldnames_vl = (hvl_t *)calloc((size_t)(*nfields), sizeof(*fieldnames_vl));
+        if ( fieldnames_vl == NULL ) {
+            H5Sclose(space_id);
+            H5Aclose(attr_id);
+            return MATIO_E_OUT_OF_MEMORY;
         }
+        field_id = H5Aget_type(attr_id);
+        herr = H5Aread(attr_id, field_id, fieldnames_vl);
+        if ( herr >= 0 ) {
+            matvar->internal->num_fields = (unsigned int)*nfields;
+            matvar->internal->fieldnames =
+                (char **)calloc((size_t)(*nfields), sizeof(*matvar->internal->fieldnames));
+            if ( matvar->internal->fieldnames != NULL ) {
+                for ( i = 0; i < *nfields; i++ ) {
+                    matvar->internal->fieldnames[i] = (char *)calloc(fieldnames_vl[i].len + 1, 1);
+                    if ( matvar->internal->fieldnames[i] != NULL ) {
+                        if ( fieldnames_vl[i].p != NULL ) {
+                            memcpy(matvar->internal->fieldnames[i], fieldnames_vl[i].p,
+                                   fieldnames_vl[i].len);
+                        }
+                    } else {
+                        err = MATIO_E_OUT_OF_MEMORY;
+                        break;
+                    }
+                }
+            } else {
+                err = MATIO_E_OUT_OF_MEMORY;
+            }
 #if H5_VERSION_GE(1, 12, 0)
-        H5Treclaim(field_id, space_id, H5P_DEFAULT, fieldnames_vl);
+            H5Treclaim(field_id, space_id, H5P_DEFAULT, fieldnames_vl);
 #else
-        H5Dvlen_reclaim(field_id, space_id, H5P_DEFAULT, fieldnames_vl);
+            H5Dvlen_reclaim(field_id, space_id, H5P_DEFAULT, fieldnames_vl);
 #endif
-    } else {
-        err = MATIO_E_GENERIC_READ_ERROR;
+            free(fieldnames_vl);
+            H5Tclose(field_id);
+        } else {
+            err = MATIO_E_GENERIC_READ_ERROR;
+        }
     }
 
     H5Sclose(space_id);
-    H5Tclose(field_id);
     H5Aclose(attr_id);
-    free(fieldnames_vl);
 
     return err;
 }
