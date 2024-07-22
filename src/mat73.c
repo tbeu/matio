f07ede848d8ab17a1f3a330073f1795a712a3f2c
tbeu <tbeu@users.noreply.github.com>
2024-07-22 18:24:30 +0200

    Fix resource leak
    As reported by Coverity Scan

--- src/mat73.c
+++ src/mat73.c
@@ -465,8 +465,10 @@ Mat_H5ReadVarInfo(matvar_t *matvar, hid_t dset_id)
 
     matvar->internal->id = dset_id;
     attr_id = H5Aopen_by_name(dset_id, ".", "MATLAB_class", H5P_DEFAULT, H5P_DEFAULT);
-    if ( attr_id < 1 )
+    if ( attr_id < 1 ) {
+        H5Aclose(attr_id);
         return MATIO_E_FAIL_TO_IDENTIFY;
+    }
     type_id = H5Aget_type(attr_id);
     class_str = (char *)calloc(H5Tget_size(type_id) + 1, 1);
     if ( NULL != class_str ) {
