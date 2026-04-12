9cbc2517aeda272138f35091448d08f11c7cdc42
tbeu <tbeu@users.noreply.github.com>
2026-04-12 19:29:21 +0200

    Fix endianness detection for MSVC on ARM/ARM64 [skipp appveyor]
    As reported by https://github.com/tbeu/matio/issues/287

--- src/matio_private.h
+++ src/matio_private.h
@@ -90,10 +90,11 @@
     defined(__ppc__) || defined(__hpux) || defined(_MIPSEB) || defined(_POWER) ||          \
     defined(__s390__)
 #define MATIO_BE
-#elif defined(__i386__) || defined(__alpha__) || defined(__ia64) || defined(__ia64__) ||   \
-    defined(_M_IX86) || defined(_M_IA64) || defined(_M_ALPHA) || defined(__amd64) ||       \
-    defined(__amd64__) || defined(_M_AMD64) || defined(__x86_64) || defined(__x86_64__) || \
-    defined(_M_X64) || defined(__bfin__) || defined(__loongarch64) || defined(__aarch64__)
+#elif defined(__i386__) || defined(__alpha__) || defined(__ia64) || defined(__ia64__) ||      \
+    defined(_M_IX86) || defined(_M_IA64) || defined(_M_ALPHA) || defined(__amd64) ||          \
+    defined(__amd64__) || defined(_M_AMD64) || defined(__x86_64) || defined(__x86_64__) ||    \
+    defined(_M_X64) || defined(__bfin__) || defined(__loongarch64) || defined(__aarch64__) || \
+    defined(_M_ARM64) || defined(_M_ARM)
 #define MATIO_LE
 #endif
 
