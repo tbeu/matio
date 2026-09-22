3195f7a4a8ff7bbcefb66bfa11741d4b40af5302
Fabian Keßler-Schulz <fabian_kessler@gmx.de>
2026-09-22 11:25:04 +0200

    Make install targets optional Using matio and hdf5 as subproject has the problem, that neither hdf5 nor zlib are in a export set, but matio now expects that. This change makes that behaviour optional / controllable.

--- cmake/src.cmake
+++ cmake/src.cmake
@@ -132,6 +132,12 @@ if(STDINT_MSVC)
 endif()
 set_target_properties(${PROJECT_NAME} PROPERTIES PUBLIC_HEADER "${MATIO_PUBLIC_HEADERS}")
 
+include(CMakeDependentOption)
+cmake_dependent_option(MATIO_INSTALL "Whether to install matio" ON PROJECT_IS_TOP_LEVEL OFF)
+if(NOT MATIO_INSTALL)
+    return()
+endif()
+
 # 'make install' to the correct locations (provided by GNUInstallDirs).
 install(TARGETS ${PROJECT_NAME} EXPORT lib${PROJECT_NAME}
         PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
